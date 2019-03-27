f32e75dd385bbe36b49758834192e1f1f4cde3f4
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-27 18:13:17 +0100

    Fixes logging typo

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -1,15 +1,15 @@
 const checkObservatory = require('../lib/check-observatory')
 
 module.exports = async function (context, mySbMsg) {
-  context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - start`)
+  context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - start`)
   const host = mySbMsg.host
   try {
-    context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - observatory - start`)
+    context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - start`)
     let result = await checkObservatory(context, host)
-    context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - observatory - success`)
+    context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - success`)
     context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'observatory', result: result })
-    context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - finished`)
+    context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - finished`)
   } catch (error) {
-    context.log.error(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - ${error}`)
+    context.log.error(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - ${error}`)
   }
 }
