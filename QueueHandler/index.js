9af8b4726625a4a1d3ce39857e1d8689270dcc51
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-27 07:16:44 +0100

    Implements check-observatory

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -1,11 +1,11 @@
-const checkLighthouse = require('../lib/check-lighthouse')
+const checkObservatory = require('../lib/check-observatory')
 
 module.exports = async function (context, mySbMsg) {
   context.log(`job received - ${mySbMsg.id} - start`)
   const pageUrl = mySbMsg.url
   try {
     context.log(`job - ${mySbMsg.id} - url - ${pageUrl} - lookup - start`)
-    let result = await checkLighthouse(context, pageUrl)
+    let result = await checkObservatory(context, pageUrl)
     context.log(`job - ${mySbMsg.id} - url - ${pageUrl} - lookup - success`)
     context.bindings.mySbQueue = Object.assign({}, mySbMsg, { result: result })
     context.log(`job - ${mySbMsg.id} - finished`)
