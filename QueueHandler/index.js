d9d68c615062cea30b48696874fc3f330756c1d0
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-27 18:12:19 +0100

    Updates return message

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, mySbMsg) {
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - observatory - start`)
     let result = await checkObservatory(context, host)
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - observatory - success`)
-    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { result: result })
+    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'observatory', result: result })
     context.log(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - finished`)
   } catch (error) {
     context.log.error(`azf-shared-lighthouse - ${mySbMsg.eventSourceId} - host - ${host} - ${error}`)
