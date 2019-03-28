158feb0ae5527b233a8fc808364d9821577024ed
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-28 08:55:18 +0100

    Wraps check in a Promise

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, mySbMsg) {
   const host = mySbMsg.host
   try {
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - start`)
-    let result = await checkObservatory(context, host)
+    const result = await checkObservatory(context, host)
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - success`)
     context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'observatory', result: result })
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - finished`)
