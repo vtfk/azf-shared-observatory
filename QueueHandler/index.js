d78a04786ad95ff8c285df1113fbd4b59d7cba6d
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-29 07:21:01 +0100

    Adds timeStamp

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, mySbMsg) {
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - start`)
     const result = await checkObservatory(context, host)
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - observatory - success`)
-    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'observatory', date: new Date(), result: result })
+    context.bindings.mySbQueue = Object.assign({}, mySbMsg, { domain: 'webquality', category: 'observatory', date: new Date(), timeStamp: new Date().getTime(), result: result })
     context.log(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - finished`)
   } catch (error) {
     context.log.error(`azf-shared-observatory - ${mySbMsg.eventSourceId} - host - ${host} - ${error}`)
